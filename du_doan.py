23dddfb217ee8321f3d8c24db114a26470a0ca0e
PhanDuyHieu <phanduyhieu28122002@gmail.com>
2023-10-10 17:22:12 +0700

    update

--- du_doan.py
+++ du_doan.py
@@ -1,32 +1,38 @@
 import numpy as np
 import tensorflow as tf
-import pandas as pd
 
-# Đọc dữ liệu từ tệp CSV
-data=pd.read_csv('Student_Performance.csv') # Thay 'du_lieu.csv' bằng đường dẫn tới tệp CSV của bạn
-x = np.array(data.iloc[:, :5])  # Lấy dữ liệu từ 5 cột đầu tiên
-y = np.array(data.iloc[:, 5])  # Lấy dữ liệu từ cột cuối cùng
+# Đọc dữ liệu từ file
+data = np.loadtxt('Student_Performance.csv', delimiter=',', skiprows=1)
+
+# Chia dữ liệu thành features (đặc trưng) và labels (nhãn)
+features = data[:, :-1]
+labels = data[:, -1]
 
 # Chuẩn hóa dữ liệu
-x = (x - np.mean(x, axis=0)) / np.std(x, axis=0)
+features = (features - np.mean(features, axis=0)) / np.std(features, axis=0)
 
-# Chia dữ liệu thành tập huấn luyện và tập kiểm tra (80% - 20%)
-split_point = int(0.5 * len(data))
-training_x, test_x = x[:split_point], x[split_point:]
-training_y, test_y = y[:split_point], y[split_point:]
+# Chia dữ liệu thành tập huấn luyện và tập kiểm tra
+train_ratio = 0.3
+train_size = int(train_ratio * len(data))
 
-# Thiết lập mô hình TensorFlow
+train_features = features[:train_size]
+train_labels = labels[:train_size]
+test_features = features[train_size:]
+test_labels = labels[train_size:]
+
+# Xây dựng mô hình neural network
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=(5,)),
     tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
+# Compile mô hình
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Huấn luyện mô hình
-model.fit(training_x, training_y, epochs=10, batch_size=32)
+model.fit(train_features, train_labels, epochs=10, batch_size=32, verbose=1)
 
 # Đánh giá mô hình trên tập kiểm tra
-loss, accuracy = model.evaluate(test_x, test_y)
-print("Độ chính xác trên tập kiểm tra:", accuracy)  
+loss, accuracy = model.evaluate(test_features, test_labels, verbose=0)
+print('Accuracy:', accuracy)
