616f90bf339a2d1e2deffb8ab62617db4f272e72
hoangdinhhieuk15 <145357254+hoangdinhhieuk15@users.noreply.github.com>
2023-10-17 11:18:31 +0700

    Update buoi6_bai3.py
    chuan hoa hinh anh
    
    Signed-off-by: hoangdinhhieuk15 <145357254+hoangdinhhieuk15@users.noreply.github.com>

---
+++ buoi6_bai3.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read the image
+img = cv2.imread('test.png')
+
+# Convert the image to grayscale
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Normalize the grayscale image
+normalized_gray_img = cv2.normalize(gray_img, None, 0, 255, cv2.NORM_MINMAX)
+
+# Display the original image
+plt.subplot(1, 2, 1)
+plt.title("Original")
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.axis('off')
+
+# Display the normalized image
+plt.subplot(1, 2, 2)
+plt.title("Normalized")
+plt.imshow(normalized_gray_img, cmap='gray')
+plt.axis('off')
+
+plt.show()
