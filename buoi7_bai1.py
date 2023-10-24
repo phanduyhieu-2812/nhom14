2d5b498981c62de055a0ae672b25f61c017f587b
TrungHieu0 <146054601+TrungHieu0@users.noreply.github.com>
2023-10-24 22:36:46 +0700

    Update buoi7_bai1.py
    Signed-off-by: TrungHieu0 <146054601+TrungHieu0@users.noreply.github.com>

--- buoi7_bai1.py
+++ buoi7_bai1.py
@@ -4,7 +4,6 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
-
 # Hàm xử lý sự kiện khi nút "Open Image" được nhấn
 def open_image():
     global img, original_img
@@ -14,25 +13,6 @@ def open_image():
     original_img = img.copy()
     display_image(img)
 
-
-# Hàm xử lý sự kiện khi nút "Apply Filters" được nhấn
-def apply_filters():
-    global img
-    kernel_sharpen_1 = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-    kernel_sharpen_2 = np.array([[1, 1, 1], [1, -7, 1], [1, 1, 1]])
-    kernel_sharpen_3 = np.array([[-1, -1, -1, -1, -1],
-                                 [-1, 2, 2, 2, -1],
-                                 [-1, 2, 8, 2, -1],
-                                 [-1, 2, 2, 2, -1],
-                                 [-1, -1, -1, -1, -1]]) / 8.0
-
-    output_1 = cv2.filter2D(img, -1, kernel_sharpen_1)
-    output_2 = cv2.filter2D(img, -1, kernel_sharpen_2)
-    output_3 = cv2.filter2D(img, -1, kernel_sharpen_3)
-
-    display_image(output_1, 'Sharpening')
-    display_image(output_2, 'Excessive Sharpening')
-    display_image(output_3, 'Edge Enhancement')
 # Hàm xử lý sự kiện khi nút "Save Image" được nhấn
 def save_image():
     global img
@@ -40,13 +20,38 @@ def save_image():
     if file_path:
         cv2.imwrite(file_path, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
 
-
-# Hàm xử lý sự kiện khi nút "Undo" được nhấn
-def undo_filters():
+# Hàm xử lý sự kiện khi nút "Apply Blur" được nhấn
+def apply_blur():
     global img
-    img = original_img.copy()
-    display_image(img)
+    blurred_img = cv2.GaussianBlur(img, (21, 21), 0)  # Áp dụng bộ lọc Gaussian Blur
+    display_image(blurred_img, 'Blurred Image')
 
+# Hàm xử lý sự kiện khi nút "Draw" được nhấn
+def draw_on_image():
+    global img
+    drawing = False
+    last_x, last_y = 0, 0
+
+    def draw(event):
+        nonlocal drawing, last_x, last_y
+        if drawing:
+            x, y = event.x, event.y
+            cv2.line(img, (last_x, last_y), (x, y), (0, 255, 0), 5)
+            last_x, last_y = x, y
+            display_image(img, 'Drawing on Image')
+
+    def start_draw(event):
+        nonlocal drawing, last_x, last_y
+        drawing = True
+        last_x, last_y = event.x, event.y
+
+    def stop_draw(event):
+        nonlocal drawing
+        drawing = False
+
+    label.bind("<B1-Motion>", draw)  # Xử lý sự kiện vẽ khi chuột di chuyển
+    label.bind("<Button-1>", start_draw)  # Xử lý sự kiện bắt đầu vẽ khi nhấn chuột trái
+    label.bind("<ButtonRelease-1>", stop_draw)  # Xử lý sự kiện kết thúc vẽ khi thả chuột trái
 
 # Hàm hiển thị hình ảnh trong cửa sổ
 def display_image(image, window_title='Image'):
@@ -56,26 +61,25 @@ def display_image(image, window_title='Image'):
     label.image = photo
     root.title(window_title)
 
-
 # Tạo cửa sổ Tkinter
 root = tk.Tk()
-root.title("Image Filters")
+root.title("Image Processing App")
 
 # Tạo nút để mở hình ảnh
 open_button = tk.Button(root, text="Open Image", command=open_image)
 open_button.pack(pady=10)
 
-# Tạo nút để áp dụng bộ lọc
-apply_button = tk.Button(root, text="Apply Filters", command=apply_filters)
-apply_button.pack(pady=10)
 # Tạo nút để lưu hình ảnh
 save_button = tk.Button(root, text="Save Image", command=save_image)
 save_button.pack(pady=10)
 
+# Tạo nút để áp dụng bộ lọc làm mờ
+blur_button = tk.Button(root, text="Apply Blur", command=apply_blur)
+blur_button.pack(pady=10)
 
-# Tạo nút để hoàn tác bộ lọc
-undo_button = tk.Button(root, text="Undo Filters", command=undo_filters)
-undo_button.pack(pady=10)
+# Tạo nút để vẽ lên hình ảnh
+draw_button = tk.Button(root, text="Draw", command=draw_on_image)
+draw_button.pack(pady=10)
 
 # Label để hiển thị hình ảnh
 label = tk.Label(root)
